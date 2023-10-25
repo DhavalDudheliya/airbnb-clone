46e24ee7ec8481ab6e4fb0951731ba19c55f4493
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2023-10-25 17:12:13 +0530

    bug

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 // app.use(
 //   cors({
 //     credentials: true,
-//     // origin: "http://localhost:5173",
+//     /origin: "http://localhost:5173",
 //     origin: "https://venerable-bombolone-64d854.netlify.app",
 //   })
 // );
